97f20013551debee8769ee2fb0868f7f16ad0f91
Paulo Carvalho <paulocarvalhofrediani@gmail.com>
2013-02-25 19:32:54 +0000

    no message

---
+++ public/ckeditor/config.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+    config.uiColor = '#AADC6E';
+	config.extraPlugins = 'internpage';
+    config.removePlugins = 'devtools';
+    config.toolbar_Basic =
+          [
+            ['Source','oembed','-', 'Bold', 'Italic', 'TextColor', 'BGColor', '-', 'NumberedList', 'BulletedList' ,'-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyFull', '-', 'Link', 'Unlink', "Anchor", 'Image','HorizontalRule','Styles','Format','Font','FontSize' ]
+      ];
+    config.toolbar = 'Basic'; 
+};
